fa262e87a2749628db233feb9a4640b8eecb33d6
JaneJung <piaojane@daum.net>
2021-03-31 10:27:19 +0900

    Calculating the Distance Between Two 3D points

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotNodes = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -69,8 +71,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             hitResult.worldTransform.columns.3.z)
 
         sceneView.scene.rootNode.addChildNode(dotNode)
-    
+        
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculate()
+        }
     }
+        
+        func calculate() {
+            let start = dotNodes[0]
+            let end = dotNodes[1]
+            
+            print(start.position)
+            print(end.position)
+            
+            let distance = sqrt(
+                pow(end.position.x - start.position.x, 2) + pow(end.position.y - start.position.y, 2) + pow(end.position.z - start.position.z, 2)
+            )
+            
+            print(abs(distance))
+            
+        }
+    
+    
 
 
 }
